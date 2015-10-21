4bd17abfbc980d6c4af1724cf7f78f5e3c4396cd
Demian Katz <demian.katz@villanova.edu>
2015-10-21 11:47:20 -0400

    Made jobs visible.

--- app/assets/javascripts/client.js
+++ app/assets/javascripts/client.js
@@ -3,12 +3,25 @@ var VuDLPrep = {
         this.url = url;
         this.container = container;
         this.buildJobSelector();
+        this.buildPaginator();
     },
 
     buildJobSelector: function () {
         this.jobSelector = $('<div id="jobSelector"></div>');
         this.container.append(this.jobSelector);
         this.fetchJobs(this.jobSelector);
+    },
+
+    buildPaginator: function() {
+        this.paginator = $('<div id="paginator"></div>');
+        this.paginatorList = $('<div class="pageList"></div>');
+        this.paginator.append(this.paginatorList);
+        this.paginatorPreview = $('<div class="preview"></div>');
+        this.paginator.append(this.paginatorPreview);
+        this.paginatorControls = $('<div class="controls"></div>');
+        this.paginator.append(this.paginatorControls);
+        this.paginator.hide();
+        this.container.append(this.paginator);
     },
 
     fetchJobs: function(target) {
@@ -46,7 +59,68 @@ var VuDLPrep = {
         }
     },
 
+    getPageSelector: function(p) {
+        var that = this;
+        return function () {
+            return that.selectPage(p);
+        }
+    },
+
     selectJob: function(category, job) {
-        console.log(category, job);
+        this.currentCategory = category;
+        this.currentJob = job;
+        this.jobSelector.hide();
+        this.paginator.show();
+
+        this.loadPageList(category, job, this.paginatorList);
+    },
+
+    getImageUrl: function(category, job, filename, size) {
+        return this.getJobUrl(
+            category, job,
+            '/' + encodeURIComponent(filename) + '/' + encodeURIComponent(size)
+        );
+    },
+
+    getJobUrl: function(category, job, extra) {
+        return this.url + '/' + encodeURIComponent(category) + '/'
+            + encodeURIComponent(job) + extra;
+    },
+
+    loadPageList: function(category, job, target) {
+        var that = this;
+        target.empty();
+        jQuery.getJSON(this.getJobUrl(category, job, '/pagination'), null, function (data, status) {
+            that.currentPageOrder = data['order'];
+            that.thumbnails = [];
+            for (var i = 0; i < data['order'].length; i++) {
+                var currentThumb = $('<div class="thumbnail"></div>');
+                currentThumb.click(that.getPageSelector(i));
+                var currentImage = $('<img />');
+                currentImage.attr('src', that.getImageUrl(category, job, data['order'][i]['filename'], 'thumb'))
+                currentThumb.append(currentImage);
+                var currentLabel = $('<div class="label"></div>').text(data['order'][i]['label']);
+                currentThumb.append(currentLabel);
+                target.append(currentThumb);
+                that.thumbnails[i] = currentThumb;
+            }
+            that.selectPage(0);
+        });
+    },
+
+    loadPreview: function(category, job, filename) {
+        var currentImage = $('<img />');
+        currentImage.attr(
+            'src', this.getImageUrl(category, job, filename, 'medium')
+        );
+        this.paginatorPreview.empty();
+        this.paginatorPreview.append(currentImage);
+    },
+
+    selectPage: function(p) {
+        this.currentPage = p;
+        this.loadPreview(
+            this.currentCategory, this.currentJob, this.currentPageOrder[p]['filename']
+        );
     }
 };
