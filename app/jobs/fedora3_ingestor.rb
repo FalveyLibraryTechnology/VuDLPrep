d4013f5845809df8403366690e02d5b6f61409f7
Demian Katz <demian.katz@villanova.edu>
2015-11-16 16:04:11 -0500

    Fedora 3 ingest (work in progress).

--- app/jobs/fedora3_ingestor.rb
+++ app/jobs/fedora3_ingestor.rb
@@ -17,6 +17,20 @@ class Fedora3Ingestor
     end
 
     resource = Fedora3Object.from_next_pid
-    logger.info resource.pid
+    resource.logger = logger
+    resource.parent_pid = holding_area.pid
+    resource.model_type = "ResourceCollection"
+    resource.title = "Incomplete... / Processing..."
+    logger.info "Creating Resource Object #{resource.pid}"
+
+    resource.core_ingest("I")
+    resource.collection_ingest
+    resource.resource_collection_ingest
+
+    if (member_position > 0)
+      raise "TODO: deal with ordered collection sequence numbers"
+    end
+  
+    logger.info "Done."
   end
 end
--- app/models/fedora3_object.rb
+++ app/models/fedora3_object.rb
@@ -2,7 +2,7 @@ require 'rexml/document'
 require 'rexml/xpath'
 
 class Fedora3Object
-  attr_accessor :pid
+  attr_accessor :logger, :model_type, :parent_pid, :pid, :title
 
   def initialize()
     @config = Rails.application.config_for(:vudl)
@@ -20,6 +20,40 @@ class Fedora3Object
     obj
   end
 
+  def add_datastream(ds_id, control_group, ds_location, alt_ids, ds_label, versionable, ds_state, format_uri, checksum_type, checksum, mime_type, log_message, data)
+    log "Adding datastream #{ds_id} to #{pid}"
+    uri = URI("#{api_base}/objects/#{pid}/datastreams/#{ds_id}")
+    params = {
+      controlGroup: control_group,
+      dsLocation: ds_location,
+      altIDs: alt_ids,
+      dsLabel: ds_label,
+      versionable: versionable,
+      dsState: ds_state,
+      formatURI: format_uri,
+      checksumType: checksum_type,
+      checksum: checksum_type == 'DISABLED' ? nil : checksum,
+      mimeType: mime_type,
+      logMessage: log_message
+    }
+    uri.query = URI.encode_www_form(params)
+    response = do_post(uri, data)
+  end
+
+  def add_relationship(subject, predicate, object, is_literal, datatype)
+    log "Adding relationship #{subject} #{predicate} #{object} to #{pid}"
+    uri = URI("#{api_base}/objects/#{pid}/relationships/new")
+    params = {
+      subject: subject,
+      predicate: predicate,
+      object: object,
+      isLiteral: is_literal,
+      datatype: datatype
+    }
+    uri.query = URI.encode_www_form(params)
+    response = do_post(uri)
+  end
+
   def api_base
     @config["fedora3_api"]
   end
@@ -32,12 +66,185 @@ class Fedora3Object
     @config["fedora3_username"]
   end
 
+  def collection_ingest
+    log "Collection ingest for #{pid}"
+    add_relationship(
+      "info:fedora/#{pid}",
+      'info:fedora/fedora-system:def/model#hasModel',
+      'info:fedora/vudl-system:CollectionModel',
+      'false',
+      nil
+    )
+    add_datastream(
+      'MEMBER-QUERY',
+      'E',
+      "http://local.fedora.server/fedora/objects/#{pid}/methods/vudl-system:CollectionModelService/generateMemberQuery",
+      nil,
+      'Query to list members', 
+      'false', 
+      'A', 
+      nil, 
+      'DISABLED', 
+      'none', 
+      'text/plain', 
+      'Initial Ingest addDatastream - MEMBER-QUERY',
+      nil
+    )
+    add_datastream(
+      'MEMBER-LIST-RAW',
+      'E',
+      "http://local.fedora.server/fedora/objects/#{pid}/methods/vudl-system:CollectionModelService/executeMemberQuery",
+      nil,
+      'Unformatted list of Members', 
+      'false', 
+      'A', 
+      nil, 
+      'DISABLED', 
+      'none', 
+      'text/xml', 
+      'Initial Ingest addDatastream - MEMBER-LIST-RAW',
+      nil
+    )
+  end
+
+  def core_ingest(object_state)
+    log "Core ingest for #{pid}"
+    ingest(
+      title,
+      'info:fedora/fedora-system:FOXML-1.1',
+      'UTF-8',
+      namespace,
+      'diglibEditor',
+      title + ' - ingest',
+      'false'
+    )
+    modify_object(nil, nil, object_state, 'Set initial state', nil)
+    add_relationship(
+      "info:fedora/#{pid}",
+      'info:fedora/fedora-system:def/model#hasModel',
+      'info:fedora/vudl-system:CoreModel',
+      'false',
+      nil
+    )
+    add_relationship(
+      "info:fedora/#{pid}",
+      'info:fedora/fedora-system:def/relations-external#isMemberOf',
+      "info:fedora/#{parent_pid}",
+      'false',
+      nil
+    )
+    add_datastream(
+      'PARENT-QUERY',
+      'E',
+      "http://local.fedora.server/fedora/objects/#{pid}/methods/vudl-system:CoreModelService/generateParentQuery",
+      nil,
+      'Query to list Parents', 
+      'false', 
+      'A', 
+      nil, 
+      'DISABLED', 
+      'none', 
+      'text/plain', 
+      'Initial Ingest addDatastream - PARENT-QUERY',
+      nil
+    )
+    add_datastream(
+      'PARENT-LIST-RAW',
+      'E',
+      "http://local.fedora.server/fedora/objects/#{pid}/methods/vudl-system:CoreModelService/executeParentQuery",
+      nil,
+      'Unformatted list of Parents', 
+      'false', 
+      'A', 
+      nil, 
+      'DISABLED', 
+      'none', 
+      'text/xml', 
+      'Initial Ingest addDatastream - PARENT-LIST-RAW',
+      nil
+    )
+    add_datastream(
+      'PARENT-LIST',
+      'E',
+      "http://local.fedora.server/fedora/objects/#{pid}/methods/vudl-system:CoreModelService/formatParentQueryResult",
+      nil,
+      'XML list of parents grouped by multiple paths', 
+      'false', 
+      'A', 
+      nil, 
+      'DISABLED', 
+      'none', 
+      'text/xml', 
+      'Initial Ingest addDatastream - PARENT-LIST',
+      nil
+    )
+  end
+
   def datastream_dissemination(datastream, as_of_data_time = nil, download = nil)
     uri = URI("#{api_base}/objects/#{pid}/datastreams/#{datastream}/content")
     params = { :asOfDataTime => as_of_data_time, :download => download }
     uri.query = URI.encode_www_form(params)
     response = Net::HTTP.get_response(uri)
     response.body
+  end
+
+  def do_post(uri, body = nil, mime = nil)
+    req = Net::HTTP::Post.new(uri)
+    req.basic_auth api_username, api_password
+    req.body = body
+    if (mime)
+      req.add_field('Content-Type', mime)
+    end
+    response = Net::HTTP.new(uri.host, uri.port).start {|http| http.request(req)}
+    error = false
+    if (!response)
+      error = "No response to POST"
+    end
+    if (response && response.code[0] != "2")
+      error = "Unexpected response: #{response.code} #{response.body}"
+    end
+    if (error)
+      log error
+      raise error
+    end
+    response
+  end
+
+  def ingest(label, format, encoding, namespace, owner_id, log_message, ignore_mime, xml = nil)
+    params = {
+      label: label,
+      format: format,
+      encoding: encoding,
+      namespace: namespace,
+      ownerId: owner_id,
+      logMessage: log_message,
+      ignoreMime: ignore_mime
+    }
+    target_pid = xml ? "new" : pid
+    log "Ingest for #{target_pid}"
+    uri = URI("#{api_base}/objects/#{target_pid}")
+    uri.query = URI.encode_www_form(params)
+    response = do_post(uri, xml, 'text/xml')
+ end
+
+  def log(msg)
+    if (logger)
+      logger.info msg
+    end
+  end
+
+  def modify_object(label, owner_id, state, log_message, last_modified_date)
+    log "Modifying #{pid}"
+    uri = URI("#{api_base}/objects/#{pid}")
+    params = {
+      label: label,
+      ownerId: owner_id,
+      state: state,
+      logMessage: log_message,
+      lastModifiedDate: last_modified_date
+    }
+    uri.query = URI.encode_www_form(params)
+    response = do_post(uri, nil, 'text/xml')
   end
 
   def namespace
@@ -47,11 +254,39 @@ class Fedora3Object
   def next_pid
     uri = URI("#{api_base}/objects/nextPID")
     uri.query = URI.encode_www_form({ 'namespace' => namespace, 'format' => 'xml' })
-    req = Net::HTTP::Post.new(uri)
-    req.basic_auth api_username, api_password
-    response = Net::HTTP.new(uri.host, uri.port).start {|http| http.request(req)}
+    response = do_post(uri)
     xml = REXML::Document.new(response.body)
     ns = { 'management' => 'http://www.fedora.info/definitions/1/0/management/' }
     REXML::XPath.first(xml, "//management:pid[1]/text()", ns)
+  end
+
+  def resource_collection_ingest(raw_resource = nil, license_str = nil, agents = nil, process_md = nil)
+    log "Resource collection ingest for #{pid}"
+    add_relationship(
+      "info:fedora/#{pid}",
+      'info:fedora/fedora-system:def/model#hasModel',
+      'info:fedora/vudl-system:ResourceCollection',
+      'false',
+      nil
+    )
+    add_relationship(
+      "info:fedora/#{pid}",
+      'http://vudl.org/relationships#sortOn',
+      'title',
+      'true',
+      nil
+    )
+    if (raw_resource)
+      raise "TODO: support raw_resource parameter"
+    end
+    if (license_str)
+      raise "TODO: support license_str parameter"
+    end
+    if (agents)
+      raise "TODO: support raise parameter"
+    end
+    if (process_md)
+      raise "TODO: support process_md parameter"
+    end
   end
 end
