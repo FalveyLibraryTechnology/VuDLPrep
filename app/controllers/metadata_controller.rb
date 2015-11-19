d45a0db7650cc933105d68c00fe107210164566e
Demian Katz <demian.katz@villanova.edu>
2015-11-19 14:17:24 -0500

    Simplified code with before_action.

--- app/controllers/metadata_controller.rb
+++ app/controllers/metadata_controller.rb
@@ -1,58 +1,49 @@
 require 'fileutils'
 
 class MetadataController < ApplicationController
+  before_filter :job_exists
+
   def index
-    dir = job_path(params)
-    if !Dir.exist?(dir)
-      render status: 404, json: { error: "Job not found" }
-    else
-      job = Job.new dir
-      render json: job.metadata.raw
-    end
+    job = Job.new job_path(params)
+    render json: job.metadata.raw
   end
 
   def status
-    dir = job_path(params)
-    if !Dir.exist?(dir)
-      render status: 404, json: { error: "Job not found" }
-    else
-      job = Job.new dir
-      render json: job.metadata.status
-    end
+    job = Job.new job_path(params)
+    render json: job.metadata.status
   end
 
   def make_derivatives
-    dir = job_path(params)
-    if !Dir.exist?(dir)
-      render status: 404, json: { error: "Job not found" }
-    else
-      job = Job.new dir
-      status = job.metadata.derivative_status
-      lockfile = job.metadata.derivative_lockfile
-      if (status[:expected] > status[:processed] && !File.exist?(lockfile))
-        FileUtils.touch lockfile
-        Resque.enqueue(DerivativeGenerator, dir)
-      end
-      render json: status
+    job = Job.new job_path(params)
+    status = job.metadata.derivative_status
+    lockfile = job.metadata.derivative_lockfile
+    if (status[:expected] > status[:processed] && !File.exist?(lockfile))
+      FileUtils.touch lockfile
+      Resque.enqueue(DerivativeGenerator, job.dir)
     end
+    render json: status
   end
 
   def update
+    job = Job.new job_path(params)
+    if !JobMetadataValidator.valid?(job, params)
+      render status: 400, json: { error: "Invalid JSON document" }
+    else
+      job.metadata.raw = params
+      job.metadata.save
+      if (params[:published])
+        Resque.enqueue(Fedora3Ingestor, job.dir)
+      end
+      render json: { status: 'ok' }
+    end
+  end
+
+  private
+
+  def job_exists
     dir = job_path(params)
     if !Dir.exist?(dir)
       render status: 404, json: { error: "Job not found" }
-    else
-      job = Job.new dir
-      if !JobMetadataValidator.valid?(job, params)
-        render status: 400, json: { error: "Invalid JSON document" }
-      else
-        job.metadata.raw = params
-        job.metadata.save
-        if (params[:published])
-          Resque.enqueue(Fedora3Ingestor, job.dir)
-        end
-        render json: { status: 'ok' }
-      end
     end
   end
 end
