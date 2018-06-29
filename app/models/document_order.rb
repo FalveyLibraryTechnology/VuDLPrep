a9dc12d6c4fb1fb811abf59e06e42d56a06b3847
Geoff <geoffrey.scholl@villanova.edu>
2018-06-29 09:05:30 -0400

    Make PDF glob case insensitive

--- app/models/document_order.rb
+++ app/models/document_order.rb
@@ -2,7 +2,7 @@ class DocumentOrder
     attr_reader :list
   
     def self.from_job(job)
-      list = Dir.glob("#{job.dir}/*.PDF").sort.map do |pdf|
+      list = Dir.glob("#{job.dir}/*.PDF", File::FNM_CASEFOLD).sort.map do |pdf|
         Document.new(File.basename(pdf), "PDF")
       end
       self.new list
