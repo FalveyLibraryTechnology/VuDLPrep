ac9619715aba82616a63001ab086fe340965f01c
Geoff <geoffrey.scholl@villanova.edu>
2018-09-25 11:53:38 -0400

    Randomize filenames

--- app/jobs/ocr_generator.rb
+++ app/jobs/ocr_generator.rb
@@ -4,10 +4,17 @@ class OcrGenerator
     def self.perform(pid)
       page = Fedora3Object.from_pid(pid)
       tiff_data = page.datastream_dissemination('MASTER')
-      image = Image.new('/tmp/ocr-file.TIFF')
-      File.open("/tmp/ocr-file.TIFF", "w:ASCII-8BIT") do |f|
+      filename = "/tmp" + random_string
+      image = Image.new(filename)
+      File.open(filename, "w:ASCII-8BIT") do |f|
         f.write tiff_data.to_s
       end
       page.add_datastream_from_file image.ocr, 'OCR-DIRTY', 'text/plain'
+      #File.delete(filename)
+    end
+  
+    protected
+    def random_string
+      @string ||= "#{SecureRandom.urlsafe_base64}.TIFF"
     end
 end
