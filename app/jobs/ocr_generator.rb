88c52cdf00f5080ace5f2b9b08de3fdd113526f7
Geoff Scholl <geoffrey.scholl@villanova.edu>
2018-09-25 11:31:30 -0400

    writing to file

--- app/jobs/ocr_generator.rb
+++ app/jobs/ocr_generator.rb
@@ -5,8 +5,8 @@ class OcrGenerator
       page = Fedora3Object.from_pid(pid)
       tiff_data = page.datastream_dissemination('MASTER')
       image = Image.new('/tmp/ocr-file.TIFF')
-      File.open("/tmp/ocr-file.TIFF", "w") do |f|
-        f.write tiff_data
+      File.open("/tmp/ocr-file.TIFF", "w:ASCII-8BIT") do |f|
+        f.write tiff_data.to_s
       end
       page.add_datastream_from_file image.ocr, 'OCR-DIRTY', 'text/plain'
     end
