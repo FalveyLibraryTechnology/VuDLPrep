2ea2f8cb8797138254937982d8a2345cc0b25b7a
Geoffsc <geoffrey.scholl@villanova.edu>
2020-02-11 09:35:25 -0500

    DC ingest code

--- app/jobs/fedora3_ingestor.rb
+++ app/jobs/fedora3_ingestor.rb
@@ -151,6 +151,11 @@ class Fedora3Ingestor
     )
 
     dc = resource.datastream_dissemination('DC')
+    replace_dcmetadata resource, dc.gsub(/Incomplete... \/ Processing.../, title), 'Set dc:title to ingest/process path'
+  end
+
+  def replace_dcmetadata resource, dc, message
+    @logger.info message
     resource.modify_datastream(
       'DC',
       nil,
@@ -162,10 +167,10 @@ class Fedora3Ingestor
       nil,
       nil,
       'text/xml',
-      'Set dc:title to ingest/process path',
+      message,
       nil,
       nil,
-      dc.gsub(/Incomplete... \/ Processing.../, title)
+      dc
     )
   end
 
@@ -211,12 +216,17 @@ class Fedora3Ingestor
       add_pages page_list
     end
 
+
     if (@job.metadata.documents.list.length > 0 || @category.supports_pdf_generation)
       document_list = build_document_list(resource)
       add_documents document_list
     end
 
     finalize_title resource
+
+    if @job.metadata.dc.length > 0
+      replace_dcmetadata resource, @job.metadata.dc, 'Loading DC XML'
+    end
 
     move_directory
 
